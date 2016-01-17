436cbbae892fa9a9fed3250ba10382e3f136e7b4
Anya Keller <anyakeller@users.noreply.github.com>
2016-01-17 16:42:12 -0500

    Update PuzzleBoard.java

--- PuzzleBoard.java
+++ PuzzleBoard.java
@@ -12,7 +12,7 @@ public class PuzzleBoard extends Rectangle implements PuzzlePlayer {
         public PuzzleBoard(int n) {
             // for us n is 3, which is the size of the board
             // set color of the rectangle (use super because we extend rectangle)
-            super();
+            super(100,500);
             // create a puzzle
             // shuffle the puzzle (1000 times!)
             // create the _tiles and add them to the PuzzleTile
