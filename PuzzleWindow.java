978bc035dbd3aae2d8815b0aa23f587909608e70
Anya Keller <anyakeller@users.noreply.github.com>
2016-01-17 16:24:03 -0500

    Update PuzzleWindow.java

--- PuzzleWindow.java
+++ PuzzleWindow.java
@@ -3,7 +3,7 @@ import wheels.users.*;
 public class PuzzleWindow extends Frame{
 
     public PuzzleWindow {
-        super;// creates the frame
+        super();// creates the frame
         PuzzleBoard board = new PuzzleBoard(3);// creates a puzzle board of size 3
         board.setLocation(100,100);// sets location of the board to (100, 100)
     }
