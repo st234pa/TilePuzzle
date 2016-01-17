43b68ea561e132703f6a64337c172b2635718979
Anya Keller <anyaisanya@gmail.com>
2016-01-17 11:21:53 -0500

    added PuzzleTile.java

---
+++ PuzzleTile.java
@@ -0,0 +1,18 @@
+import wheels.users.*;
+import java.awt.event.*;
+
+public PuzzleTile extends Image {
+
+    private PuzzleBoard _owner; // a puzzle board
+    public int _n; // the number on the tile
+
+    public PuzzleTile(PuzzleBoard owner, int n) {
+        super("images/Tile_"+n+".jpg");
+		_owner = owner;
+		_n = n;
+    }
+
+    public voud mouseClicked(MouseEvent e) {
+        _owner.tileClicked(_n); // when i click on the image, it calls the board tile clicked with the number
+    }
+}
