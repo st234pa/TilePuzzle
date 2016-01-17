b359ba980e36d52117e91e78fb22fe1b713a44a0
Stephanie <syoon1@stuy.edu>
2016-01-17 14:30:14 -0500

    finished Puzzle.java without pre, post, big O

--- Puzzle.java
+++ Puzzle.java
@@ -24,7 +24,7 @@ public class Puzzle {
 	}
 
 	public boolean isSolved() {
-		for (int i = 0; k = 1; i < size(); i++) {
+		for (int i = 0, k = 1; i < size(); i++) {
 			for (int j = 0; j < size(); j++, k++)
 				if (_puzzle[i][j] != 0 && (_puzzle[i][j] != k))
 					return false;
@@ -66,18 +66,43 @@ public class Puzzle {
 	}
 
 	public void print() {
-
+		for (int i = 0; i < size(); i++) {
+			for (int j = 0; j < size(); j++) {
+				System.out.print("[ " + at(i, j) + "] ");
+			}
+			System.out.println();
+		}
 	}
 
 	public int size() {
 		return _puzzle.length;
 	}
 
-	public boolean move(int dr, int dc) {
-
+	public boolean move(int dr, int dc) { //dr and dc would refer to the relative position of the tile that we want to move.
+		int newRow = emptyRow() + dr;
+		int newCol = emptyRow() + dc;
+		if ((newRow >= 0 && newRow < size()) && (newCol >=0 && newCol < size()) { //checks to see that the tile we want to move is on the board
+			//now switch the tile and the empty tile
+			_puzzle[emptyRow()][emptyColumn()] = _puzzle[newRow][newCol]);
+			_puzzle[newRow][newCol] = 0;
+			_emptyCellRow = newRow;
+			_emptyCellColumn = newCol;
+			return true;
+		}
+		return false;
 	}
 
-	public void shuffle(int count) {
-
+	public void shuffle(int count) { // count is the number of random moves to make
+		for (int i = 0; i < count; i++) {
+			int k = (int)(Math.random() * 4)
+			if (k = 0)
+				move(-1, 0); //move up
+			else if (k = 1)
+				move(1, 0); //move down
+			else if (k = 2)
+				move(0, -1); //move left
+			else if (k = 3)
+				move(0, 1); //move right
+		}
 	}
 }
