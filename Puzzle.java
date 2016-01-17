216dbddf05b0093721ee354305874c9a0b566440
Stephanie <syoon1@stuy.edu>
2016-01-17 16:13:02 -0500

    debugged Puzzle

--- Puzzle.java
+++ Puzzle.java
@@ -85,6 +85,7 @@ public class Puzzle {
 					return i * size() + j;
 			}
 		}
+		return -1;
 	}
 
 	//pre:
@@ -125,14 +126,14 @@ public class Puzzle {
 	//O(n)
 	public void shuffle(int count) { // count is the number of random moves to make
 		for (int i = 0; i < count; i++) {
-			int k = (int)(Math.random() * 4)
-			if (k = 0)
+			int k = (int)(Math.random() * 4);
+			if (k == 0)
 				move(-1, 0); //move up
-			else if (k = 1)
+			else if (k == 1)
 				move(1, 0); //move down
-			else if (k = 2)
+			else if (k == 2)
 				move(0, -1); //move left
-			else if (k = 3)
+			else if (k == 3)
 				move(0, 1); //move right
 		}
 	}
@@ -143,9 +144,9 @@ public class Puzzle {
 	public boolean move(int dr, int dc) { //dr and dc would refer to the relative position of the tile that we want to move.
 		int newRow = emptyRow() + dr;
 		int newCol = emptyRow() + dc;
-		if ((newRow >= 0 && newRow < size()) && (newCol >=0 && newCol < size()) { //checks to see that the tile we want to move is on the board
+		if ((newRow >= 0 && newRow < size()) && (newCol >=0 && newCol < size())) { //checks to see that the tile we want to move is on the board
 			//now switch the tile and the empty tile
-			_puzzle[emptyRow()][emptyColumn()] = _puzzle[newRow][newCol]);
+			_puzzle[emptyRow()][emptyColumn()] = _puzzle[newRow][newCol];
 			_puzzle[newRow][newCol] = 0;
 			_emptyCellRow = newRow;
 			_emptyCellColumn = newCol;
