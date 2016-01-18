f208634a1045b6612288f3fcfe67553ff147f5ea
Stephanie <syoon1@stuy.edu>
2016-01-18 16:29:36 -0500

    added comments to PuzzlePlayer.java

--- PuzzlePlayer.java
+++ PuzzlePlayer.java
@@ -1,3 +1,6 @@
 public interface PuzzlePlayer {
+    // pre condition: puzzle tiles are not solved
+    // post condition: puzzle tiles are not necessarily solved, but one tile switched places with the blank space
+    // bigO notation: WHERE IS THIS DEFINED?
         public void puzzleMove(int pos);
 }
