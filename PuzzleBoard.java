f9ccadefdea0e10c7e46560fbcfc093ecfb746d9
cdelfau <cdelfau@stuy.edu>
2016-01-18 18:20:26 -0500

    updated comments to PuzzleBoard and PuzzleSolver

--- PuzzleBoard.java
+++ PuzzleBoard.java
@@ -6,83 +6,106 @@ import java.awt.event.*;
 
 public class PuzzleBoard extends Rectangle implements PuzzlePlayer {
 
-        private ArrayList<PuzzleTile> _tiles;
+    private ArrayList<PuzzleTile> _tiles;
         
-        private Puzzle _puzzle;
-        
-        public PuzzleBoard(int n) {
-			super(Color.gray);
+    private Puzzle _puzzle;
+    
+    // pre condition: the frame has been created
+    // post condition: a board (puzzle) is created and added onto the frame
+    //                 a tile arraylist is created  and shuffled
+    // bigO notation: O(N) because we are using a for loop to go through    
+    public PuzzleBoard(int n) {
+		super(Color.gray);
 	
-			//Puzzle creation, 1000 shuffles
-			_puzzle = new Puzzle(n);
-			_puzzle.shuffle(1000);
+		//Puzzle creation, 1000 shuffles
+		_puzzle = new Puzzle(n);
+		_puzzle.shuffle(1000);
 
-			_tiles = new ArrayList<PuzzleTile>();
-			for (int i = 0;i < ( n*n -1); i++)
-				_tiles.add(new PuzzleTile(this,i+1));
+		_tiles = new ArrayList<PuzzleTile>();
+		for (int i = 0;i < ( n*n -1); i++)
+			_tiles.add(new PuzzleTile(this,i+1));
             
-			placeTiles();
+		placeTiles();
 
-			this.setSize(_tiles.get(0).getWidth() * n, _tiles.get(0).getHeight() *n); //sets the board size for the tiles
-        }
-        
-        public void placeTiles() {
-			if (_puzzle == null) return;  //for overloading
-			
+		this.setSize(_tiles.get(0).getWidth() * n, _tiles.get(0).getHeight() *n); //sets the board size for the tiles
+    }
+    
+    public void placeTiles() {
+		if (_puzzle == null) return;  //for overloading
 			placeTiles(1);
-		}
-		public void placeTiles(int n){
-			int x = getXLocation();
-			int y = getYLocation();
-			_tiles.get(n-1).setLocation(x + ((n-1)%_puzzle.size())*(_tiles.get(n-1)).getWidth(), y + ((n-1)/_puzzle.size())*(_tiles.get(n-1)).getHeight());
-			if (n < _tiles.size()) placeTiles(n +1);
-		}
+    }
 
-        public void setLocation (int x, int y) {
-            //use the super of set location and then place the tiles at (x,y)
-            super.setLocation(x,y);
-            placeTiles();
+    // pre condition: a puzzle board has been created and the tile arraylist has been created and shuffled
+    // post condition: the shuffled tiles from the arraylist are placed onto the board 
+    // bigO notation: O(N^2) 
+	public void placeTiles(int n){
+		int x = getXLocation();
+		int y = getYLocation();
+		_tiles.get(n-1).setLocation(x + ((n-1)%_puzzle.size())*(_tiles.get(n-1)).getWidth(), y + ((n-1)/_puzzle.size())*(_tiles.get(n-1)).getHeight());
+		if (n < _tiles.size()) placeTiles(n +1);
+	}
+
+    // pre condition: we have all of the tiles and the locations needed to actually place the tiles
+    // post condition: tiles are placed at the position (x,y)
+    // bigO notation: O(N^2)  
+    public void setLocation (int x, int y) {
+        //use the super of set location and then place the tiles at (x,y)
+        super.setLocation(x,y);
+        placeTiles();
+    }
+    
+    // pre condition: the tile we want to move has been clicked
+    // post condition: the tile clicked is moved to the empty space 
+    // bigO notation: O(N^2)    
+    public void tileClicked(int n) {
+        // asks the puzzle for the position of n
+        // slide(n)
+        int pos = _puzzle.pos(n);
+        slide(p);
+    }
+      
+    // pre condition: we want to move a tile
+    //                we can only move in one direction (either the row changes OR the column changes, but not both)
+    // post condition: the postion of the tile is changed and all of the tiles are replaced on the board
+    // bigO notation: O(N^2)  
+    public void slide(int p) {
+        // get the row of the position p
+        // subtract the row of the empty space
+        // get the column of the position p
+        // subtract the column of the empty space 
+        // call move    
+        int dr = _puzzle.posRow(p) - _puzzle.emptyRow(); //position relative to the empty cell
+        int dc = _puzzle.posCol(p) - _puzzle.emptyColumn(); 
+        //have to check that the move is possible: (0, 1), (0, -1), (-1, 0), (1, 0).
+        if ((Math.abs(dr) == 1 && dc == 0) || (Math.abs(dc) == 1 && dr == 0)) {
+            _puzzle.move(dr, dc); //updates the 2d array
+            placeTiles(); //updates the visuals
+        }    
+    }
+    
+    // pre condition: the empty (gray) cell is clicked
+    // post condition: new puzzle solver is created (so, we find the most efficient solution) and then we actually play the solution
+    // bigO notation: O(1)     
+    public void mouseClicked(MouseEvent e) { //this gets called when the user clicks the empty cell (gray) which is really just the board
+        // creates a puzzle solver for that _puzzle
+        PuzzleSolver solver = new PuzzleSolver(_puzzle); //THIS STUFF WILL CHANGE IF WE MAKE A SOLVER SUPERCLASS!!!
+        // solve and check the result
+        if (solver.solve()) 
+            // if it is solved, ask the solver to play using "this" as the player
+            solver.play(this);
+    }
+
+    // pre condition: 
+    // post condition:
+    // bigO notation:         
+    public void puzzleMove(int pos) {
+        // this is called by the player to play every move
+        slide(pos);
+        try {
+            _dp.paintImmediately(_dp.getBounds());
+            Thread.sleep(200);
+        } 
+        catch (Exception e) {
         }
-        
-        public void tileClicked(int n) {
-            // asks the puzzle for the position of n
-            // slide(n)
-            int pos = _puzzle.pos(n);
-            slide(p);
-        }
-        
-        public void slide(int p) {
-            // get the row of the position p
-            // subtract the row of the empty space
-            // get the column of the position p
-            // subtract the column of the empty space 
-            // call move    
-            int dr = _puzzle.posRow(p) - _puzzle.emptyRow(); //position relative to the empty cell
-            int dc = _puzzle.posCol(p) - _puzzle.emptyColumn(); 
-            //have to check that the move is possible: (0, 1), (0, -1), (-1, 0), (1, 0).
-            if ((Math.abs(dr) == 1 && dc == 0) || (Math.abs(dc) == 1 && dr == 0)) {
-                _puzzle.move(dr, dc); //updates the 2d array
-                placeTiles(); //updates the visuals
-            }    
-        }
-        
-        public void mouseClicked(MouseEvent e) { //this gets called when the user clicks the empty cell (gray) which is really just the board
-            // creates a puzzle solver for that _puzzle
-            PuzzleSolver solver = new PuzzleSolver(_puzzle); //THIS STUFF WILL CHANGE IF WE MAKE A SOLVER SUPERCLASS!!!
-            // solve and check the result
-            if (solver.solve()) 
-                // if it is solved, ask the solver to play using "this" as the player
-                solver.play(this);
-        }
-        
-        public void puzzleMove(int pos) {
-            // this is called by the player to play every move
-            slide(pos);
-            try {
-                _dp.paintImmediately(_dp.getBounds());
-                Thread.sleep(200);
-            } 
-            catch (Exception e) {
-            }
-        }
+    }
 }
--- PuzzleSolver.java
+++ PuzzleSolver.java
@@ -5,21 +5,34 @@ public class PuzzleSolver {
     private int _count; // counts the number of moves needed to solve the puzzle
     private String _moves; // a string of the positions that we move into the empty space, separated by 2 dots
 
-
+    // pre condition: _puzzle needs to exist
+    // post condition: saves _puzzle into the variable p, which is a Puzzle
+    // bigO notation: O(1)
     public PuzzleSolver(Puzzle p) {
-        // stores the puzzle
+        _puzzle = p;
     }
 
+    // pre condition: _count needs to exist
+    // post condition: the private int _count is returned
+    // bigO notation: O(1)
     public int count() {
-        // returns the number of moves (Hint: _count)
+        return _count;
     }
 
+    // pre condition: _moves needs to exist
+    // post condition: the private int _moves is returned
+    // bigO notation: O(1) 
     public String moves() {
+        return _moves;
         // returns the string with the moves
         // we won't really used this
         // it's mainly for display/debugging 
     }
 
+    //WE NEED TO GO OVER THIS BECAUSE IT IS SORT OF CONFUSING
+    // pre condition:
+    // post condition:
+    // bigO notation: 
     private boolean solve(int count, int last, String stack) { // variable last: 0 means moved tile on top, 1 moved the tile on the right, 2 means moved tile below, and 3 means moved tile on the left
         // check if the puzzle is solved
         // if current _count is null or greater than count, then we have a better solution, so save this count into _count and stack into _moves and return true
@@ -29,6 +42,10 @@ public class PuzzleSolver {
 
     }
 
+    // GO OVER THE SOLVERS
+    // pre condition:
+    // post condition: 
+    // bigO notation:  
     public boolean solve() {
         // clears the counts
         // clears moves
@@ -36,7 +53,10 @@ public class PuzzleSolver {
         // return true if count is not null (we found something!)
         // return false otherwise
     }
-
+    
+    // pre condition: the tiles are shuffled and the user is going to click on tiles
+    // post condition: the user has clicked on a tile to move
+    // bigO notation: O(N^2) 
     public void play(PuzzlePlayer player) {
         // loops on the _moves and calls the player's puzzle move function for each of the moves
 
