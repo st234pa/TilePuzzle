a7ff5ae6bcfd8ff6c3cfbebb8c1087f231e4bb8d
Stephanie <syoon1@stuy.edu>
2016-01-18 16:34:57 -0500

    added comments

--- PuzzleWindow.java
+++ PuzzleWindow.java
@@ -3,12 +3,18 @@ import java.util.*;
 
 public class PuzzleWindow extends Frame{
 
+    // pre condition: this is the driver class, so there is no preconditon
+    // post condition: makes a new frame and new board and sets board to a given position
+    // bigO notation: O(1) 
     public PuzzleWindow(){
         super();// creates the frame
         PuzzleBoard board = new PuzzleBoard(3);// creates a puzzle board of size 3
         board.setLocation(500,200);// sets location of the board to (100, 100)
     }
 
+    // pre condition: there is no precondition
+    // post condition: makes a new puzzle window
+    // bigO notation: O(1)
     public static void main(String[] args) {
         PuzzleWindow p = new PuzzleWindow();// creates a new puzzle window
     }
