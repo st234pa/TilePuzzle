18bb4bf4d0c3922c09f493377015912d0401793a
cdelfau <cdelfau@stuy.edu>
2016-01-20 22:08:21 -0500

    Centered puzzle

--- PuzzleWindow.java
+++ PuzzleWindow.java
@@ -9,7 +9,7 @@ public class PuzzleWindow extends Frame{
     public PuzzleWindow(){
         super(); // creates the frame
         PuzzleBoard board = new PuzzleBoard(3); // creates a puzzle board of size 3
-        board.setLocation(100,100); // sets location of the board to (100, 100)
+        board.setLocation(250,150); // sets location of the board to (100, 100)
     }
 
     // pre condition: there is no precondition
