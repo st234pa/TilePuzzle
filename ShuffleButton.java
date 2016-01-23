857c719327e97231eb36bec4db9d56cf94c782fe
st234pa <syoon1@stuy.edu>
2016-01-23 22:34:32 +0000

    changed ShuffleButton

--- ShuffleButton.java
+++ ShuffleButton.java
@@ -5,14 +5,14 @@ import java.awt.event.*;
 public class ShuffleButton extends Button {
     
     //pre: none
-    //post: constructs a button
+    //post: creates button, sets image to /images/ShuffleButton.png, sets location, sets _owner to b, sets _puzzle to p
     //big O: O(1)
     public ShuffleButton(PuzzleBoard b, Puzzle p) {
         super("ShuffleButton", 30, 30, b, p); //these numbers are random, change to whatever
     }
     
-    //pre: none
-    //post: board is shuffled
+    //pre: mouse is clicked on ShuffleButton
+    //post: _puzzle is shuffled, puzzleboard updated
     //big O: O(N)
     public void mouseClicked(MouseEvent e) {
         _puzzle.shuffle(1000);
